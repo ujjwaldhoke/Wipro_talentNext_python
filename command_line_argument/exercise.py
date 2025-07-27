875f5b1685e2e696163131c3bf9f78940c289ff9
ujjwaldhoke <162492533+ujjwaldhoke@users.noreply.github.com>
2025-07-27 22:56:11 +0530

    Update exercise.py

--- command_line_argument/exercise.py
+++ command_line_argument/exercise.py
@@ -1,1 +1,12 @@
+import sys
 
+if len(sys.argv) != 3:
+    print("Please provide exactly two numbers as command line arguments.")
+    sys.exit(1)
+
+num1 = int(sys.argv[1])
+num2 = int(sys.argv[2])
+
+result = num1 + num2
+
+print("Sum =", result)
